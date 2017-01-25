2c9d0504501c9322668147c134a598ca7d6acbb1
Betterwithketchup <betterwithketchup@gmail.com>
2017-01-25 16:56:13 -0500

    weather icon

--- js/other.js
+++ js/other.js
@@ -24,7 +24,15 @@ $(function weather(){
 		method: "GET",
 		url: "https://api.darksky.net/forecast/8efbb34802b179acb15193d5ec352fd2/40.712784,-74.005941/?exclude=[hourly,minutely,daily]",
 		success:function(response){	
-			$("#weather").append("<li>"+response.currently.summary+"</li>");
+			//$("#weather").append("<li>"+response.currently.summary+"</li>");
+			$("#weather").append("<canvas id='icon1' width="+64+" height="+64+"></canvas>");
+			var skycons = new Skycons({"color": "black"});
+			console.log(response.currently.icon)
+			skycons.add("icon1", response.currently.icon);
+			// if you're using the Forecast API, you can also supply
+			// strings: "partly-cloudy-day" or "rain".
+
+  			skycons.play();
 			$("#weather").append("<li>"+response.currently.temperature+" &#x02109</li>");
 			$("#weather").append("<li>Wind Speed: "+response.currently.windSpeed+"</li>");
 			$("#weather").append("<li>Chance of Precipitation: "+response.currently.precipProbability+"%</li>");
--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
-
+/*var x = window.screen.width
+var y = window.screen.height
+width: x, height: y, */
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 600, height: 800, frame:false})
+  win = new BrowserWindow({frame:false, fullscreen: true})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
